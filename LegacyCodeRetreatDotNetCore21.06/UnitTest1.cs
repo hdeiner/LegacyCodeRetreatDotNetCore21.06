7a55efacc08a89e5e1102a800b14d2fefb99927e
hdeiner <howard.deiner@deinersoft.com>
2018-09-26 10:10:38 -0400

    Add project files.

---
+++ LegacyCodeRetreatDotNetCore21.06/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LegacyCodeRetreatDotNetCore21._06
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
